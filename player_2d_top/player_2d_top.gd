40b2a6b3b74c8a47a33fda1bddfa22434e33ec08
Sebastian Laitila <SLAITILA@uwasa.fi>
2023-04-27 15:34:23 +0300

    sadg

--- player_2d_top/player_2d_top.gd
+++ player_2d_top/player_2d_top.gd
@@ -4,6 +4,5 @@ var direction:= Vector2.ZERO
 @export var speed = 200
 func _physics_process(delta):
 	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
 	velocity = direction*speed
 	move_and_slide()
